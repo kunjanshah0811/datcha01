4d4d0561da484cda246c6c50c4c811810fefb2b8
Kunjan_Shah <76934179+kunjanshah0811@users.noreply.github.com>
2025-08-16 14:33:48 +0200

    Delete 4.Datcha_working directory

--- 4.Datcha_working/app.R
+++
@@ -1,5 +0,0 @@
-source("ui.R")      # Load the UI definition
-source("server.R")  # Load the server logic
-source("global.R")  # Load global definitions and modules
-# Run the application
-shinyApp(ui = ui, server = server)
